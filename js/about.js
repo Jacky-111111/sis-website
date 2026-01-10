044c3f65693ef9bb5b1fabaacd57cf64b23fd59a
Jacky11 <2116403107@qq.com>
2026-01-10 18:55:44 -0500

    updated

--- js/about.js
+++
@@ -1,5 +0,0 @@
-/* ============================================
-   Social Media Links (About Page)
-   ============================================ */
-// Social media links are handled directly in HTML
-// Update the href attributes in about.html with actual social media URLs
